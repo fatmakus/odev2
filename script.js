e9f43b0c17b747ae07c15017d90e1c6dce762e25
fatmakus <124739045+fatmakus@users.noreply.github.com>
2025-01-23 00:51:23 +0300

    add project file

---
+++ script.js
@@ -0,0 +1,24 @@
+function calculate(operator) {
+  
+    const input1 = parseFloat(document.getElementById('input1').value);
+    const input2 = parseFloat(document.getElementById('input2').value);
+
+    let result;
+
+    switch (operator) {
+      case '+':
+        result = input1 + input2;
+        break;
+      case '-':
+        result = input1 - input2;
+        break;
+      case '*':
+        result = input1 * input2;
+        break;
+      case '/':
+        result = input2 !== 0 ? input1 / input2 : 'undefined';
+        break;
+    }
+
+    document.getElementById('result').value = result;
+  }
