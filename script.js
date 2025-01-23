c671d3ea78354d7d36ec622435fe40f32407cfb0
fatmakus <124739045+fatmakus@users.noreply.github.com>
2025-01-24 01:24:00 +0300

    Code revision.

--- script.js
+++ script.js
@@ -1,10 +1,7 @@
 function calculate(operator) {
-  
     const input1 = parseFloat(document.getElementById('input1').value);
     const input2 = parseFloat(document.getElementById('input2').value);
-
     let result;
-
     switch (operator) {
       case '+':
         result = input1 + input2;
@@ -19,6 +16,5 @@ function calculate(operator) {
         result = input2 !== 0 ? input1 / input2 : 'Undefined';
         break;
     }
-
     document.getElementById('result').value = result;
   }
